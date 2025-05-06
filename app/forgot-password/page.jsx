361cce6dcb0ee485de9f7feee29af19de587d284
Jasper <wilsonlee0730@gmail.com>
2025-05-06 19:36:55 +0800

    update

--- app/forgot-password/page.jsx
+++ app/forgot-password/page.jsx
@@ -92,6 +92,7 @@ export default function ForgotPassword() {
                 inputMode="numeric"
                 pattern="[0-9]*"
                 value={otp}
+                onChange={handleOtpChange}
                 required
                 maxLength={6}
                 className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
